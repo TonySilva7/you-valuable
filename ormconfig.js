bf15c6f78ae217f31467eb3f533a26777b95c10d
TonySilva7 <tony.deve7oper@gmail.com>
2021-06-25 23:57:13 -0300

    feat: prepare for prod

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "migrations": ["src/database/migrations/*.ts"],
-  "entities": ["src/entities/*.ts"],
+  "migrations": ["dist/database/migrations/*.js"],
+  "entities": ["dist/entities/*.js"],
   "cli": {
     "entitiesDir": "src/entities",
     "migrationsDir": "src/database/migrations"
